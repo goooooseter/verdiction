bbe8c618b9d43ec3e681b3bf7e9b608d35d29e23
goooooseter <goooooseter@gmail.com>
2025-12-19 21:21:02 +0300

    the lastest fix auth

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -2,11 +2,7 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  })
+  const response = NextResponse.next()
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -16,20 +12,17 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-          response = NextResponse.next({
-            request,
-          })
-          cookiesToSet.forEach(({ name, value, options }) =>
+        setAll(cookies) {
+          cookies.forEach(({ name, value, options }) => {
             response.cookies.set(name, value, options)
-          )
+          })
         },
       },
     }
   )
 
-  await supabase.auth.getUser()
+  // ❗ ОБЯЗАТЕЛЬНО
+  await supabase.auth.getSession()
 
   return response
 }
